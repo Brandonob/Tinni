2f4a73bdbef4204c7d41ab551a45b6ae933e1c42
rafidhossain1995 <rafidhossain@pursuit.org>
2020-07-07 12:07:11 -0400

    create most of queries, work on delete and login

--- backend/routes/users/users.js
+++ backend/routes/users/users.js
@@ -1,1 +1,10 @@
-// This is users.js
+const users = require("express").Router();
+const {createUser, getAllUsers, loginUser, deleteUser, getSingleUser} = require ("../../queries/userQuery")
+
+users.post("/", createUser); 
+users.get("/all", getAllUsers )
+users.post("/login", loginUser)
+users.delete("/:id", deleteUser)
+users.get("/singleUser/:id", getSingleUser)
+
+module.exports = users 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,18 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Router><App/></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
