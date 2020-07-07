9bb13f7e7b88c7f2198944ed24afa77ec6a9afc6
BrandonBrown <brandonbrown@pursuit.org>
2020-07-06 21:46:55 -0400

    Add appjs file and install necessary packages as well as add gitignore

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const userRouter = require("");
+
+app.use("/users", userRouter);
+
+app.listen(port, () => {
+    console.log("App is listening on port", port);
+});
