e6578623c884ceee42e308cfca570a3c34037099
rafidhossain1995 <rafidhossain@pursuit.org>
2020-07-09 19:29:10 -0400

    user itinerary routes created

--- backend/queries/User/userQuery.js
+++ backend/queries/User/userQuery.js
@@ -1,4 +1,4 @@
-const db = require ("../db/index")
+const db = require ("../../db/index")
 
 const createUser = async(req, res, next)=>{
     try{
--- backend/routes/users/users.js
+++ backend/routes/users/users.js
@@ -1,6 +1,8 @@
 const users = require("express").Router();
-const {createUser, getAllUsers, deleteUser, getSingleUser} = require ("../../queries/userQuery")
+const userItineraries = require ("./itineraryroutes")
+const {createUser, getAllUsers, deleteUser, getSingleUser} = require ("../../queries/User/userQuery")
 
+users.use("/:id/itineraries", userItineraries)
 users.post("/", createUser); 
 users.get("/all", getAllUsers )
 users.delete("/:id", deleteUser)
