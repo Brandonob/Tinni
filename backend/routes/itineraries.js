671fbe216c6175f6370df1b670b648f03517b97d
Jay Fowler <jcafowler@gmail.com>
2020-07-11 10:14:36 -0400

    Create route to get itinerary by ID and connect it to query

--- backend/routes/itineraries.js
+++ backend/routes/itineraries.js
@@ -2,5 +2,6 @@ const itineraries = require("express").Router()
 const {getAllItin, getItinById, addItin, deleteItin, editItin} = require("../queries/itineraries.js")
 
 itineraries.get("/", getAllItin)
+itineraries.get("/:id", getItinById)
 
 module.exports = itineraries
