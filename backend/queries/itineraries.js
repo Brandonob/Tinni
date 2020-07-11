8495a0b5b3bfc68eadb3f53d825e6306909ef217
Jay Fowler <jcafowler@gmail.com>
2020-07-11 09:36:03 -0400

    Create query to fetch itinerary by ID.

--- backend/queries/itineraries.js
+++ backend/queries/itineraries.js
@@ -18,6 +18,25 @@ const getAllItin = async (req, res, next) => {
   }
 }
 
+const getItinById = async (req, res, next) => {
+  let {id} = req.params
+  let itin = await db.one("SELECT * FROM itineraries WHERE id = $1", [id])
+
+  try {
+    res.status(200).json({
+      status: "Success",
+      message: "Requested itinerary has been successfully retrieved.",
+      payload: itin
+    })
+  } catch(err) {
+    res.status(404).json({
+      status: "Error",
+      message: "Unable to retrieve requested itinerary.",
+      payload: null
+    })
+  }
+}
+
 const addItin = async (req, res, next) => {
   let data = req.body
   let itin = await db.one("INSERT INTO itineraries (user_id,itinerary_date,title) VALUES (${user_id}, ${itinerary_date}, ${title}) RETURNING *", data)
