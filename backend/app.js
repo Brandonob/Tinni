4a408062e7216fe3b7897c6d84eb09f937fb7e90
Dugmar <dugmar.morocho@gmail.com>
2020-07-11 11:15:18 -0400

    Files would be rearrange for users

--- backend/app.js
+++ backend/app.js
@@ -4,23 +4,25 @@ const cors = require("cors");
 const app = express();
 const port = 3002;
 
-
-const userRouter = require("../backend/routes/users/users");
-
+const userRouter = require("./routes/users/users");
+const activitiesRouter = require("./routes/activities");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/users", userRouter);
-app.use((err, req, res, next) => {
-    console.log(err);
-    if (err.status) {
-      res.status(err.status).json(err);
-    } else {
-      res.status(500).json(err);
-    }
-  });
+app.use("/activities", activitiesRouter);
+
+//rafid explain code vvvvv
+// app.use((err, req, res, next) => {
+//   console.log(err);
+//   if (err.status) {
+//     res.status(err.status).json(err);
+//   } else {
+//     res.status(500).json(err);
+//   }
+// });
 
 app.listen(port, () => {
   console.log("App is listening on port", port);
