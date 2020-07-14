14d43f3d3ad02fe9fe87a6bc08873bd5b1a847fa
Jay Fowler <jcafowler@gmail.com>
2020-07-14 14:30:21 -0400

    Create query to fetch itineraries by user_id and date

--- backend/queries/users/itineraries/userItineraries.js
+++ backend/queries/users/itineraries/userItineraries.js
@@ -1,7 +1,7 @@
 const db = require ("../../../db/index")
 
 
-const getUserItins = async(req, res, next)=>{
+const getUserItins = async(req, res, next)=> {
     try{
         let {id} = req.params
         const itins = await db.any("SELECT users.id AS user_id, users.first_name || ' ' || users.last_name AS full_name,itineraries.id AS itinerary_id, itineraries.title, itinerary_date, private FROM users JOIN itineraries ON users.id = itineraries.user_id WHERE users.id = $1", [id])
@@ -11,6 +11,20 @@ const getUserItins = async(req, res, next)=>{
             payload: itins
         })
     }catch(err){
+        res.status(404).json({
+            status: "Error",
+            message: "Unable to retrieve itineraries for requested user.",
+            payload: null
+        })
+    }
+}
+
+const getUserItinsByDate = async (req, res, next) => {
+    try{
+        let {date} = req.body
+        let {id} = req.params
+        const itins = await db.any ("SELECT users.id AS user_id, users.first_name || ' ' || users.last_name AS full_name,itineraries.id AS itinerary_id, itineraries.title, itinerary_date, private FROM users JOIN itineraries ON users.id = itineraries.user_id WHERE users.id = $1 AND itinerary_date = $2", [id,date])
+    } catch(err) {
         res.status(404).json({
             status: "Error",
             message: "Unable to retrieve itineraries for requested user.",
