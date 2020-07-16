b94a235184d779d577c2c4be112e345089c5fb03
Dugmar <dugmar.morocho@gmail.com>
2020-07-15 20:39:41 -0400

    homepage merge conflict resolved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,9 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Homepage from "./features/Home/home";
 
-import Login from "./features/Login/Login"
+import Login from "./features/Login/Login";
 import Signup from "./features/Login/Signup";
-import ItineraryDisplayPage from "./features/Itinerary/SearchResultDisplayPage"
-
+import ItineraryDisplayPage from "./features/Itinerary/SearchResultDisplayPage";
 
 function App() {
   return (
@@ -14,15 +13,15 @@ function App() {
       <Route exact path={"/home"}>
         <Homepage />
       </Route>
-        <Route exact path={"/itineraries"}>
-         <ItineraryDisplayPage/>
-       </Route>
-       <Route exact path={"/signup"}>
-         <SignUppage />
-       </Route>
-       <Route exact path={"/login"}>
-         <Login />
-       </Route>
+      <Route exact path={"/itineraries"}>
+        <ItineraryDisplayPage />
+      </Route>
+      <Route exact path={"/signup"}>
+        <Signup />
+      </Route>
+      <Route exact path={"/login"}>
+        <Login />
+      </Route>
     </div>
   );
 }
