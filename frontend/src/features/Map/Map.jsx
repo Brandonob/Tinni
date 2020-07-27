1fe2abb52a4da8f36eb7c1425f2ce4f6dcff6b4a
Jay Fowler <jcafowler@gmail.com>
2020-07-27 18:57:29 -0400

    Display map centered over NYC

--- frontend/src/features/Map/Map.jsx
+++ frontend/src/features/Map/Map.jsx
@@ -1,27 +1,69 @@
-import React, {Component} from 'react';
-import {Map, GoogleApiWrapper} from "google-maps-react"
+// import React, {Component} from 'react';
+// import {Map, Marker, GoogleApiWrapper} from "google-maps-react"
+// import './map.css'
 
-const mapStyles = {
-  width: "100%",
-  height: "100%"
+// const mapStyles = {
+//   width: "100%",
+//   height: "100%"
+// }
+
+// export class MapContainer extends Component {
+//   render () {
+//     return (
+//       <Map 
+//       google={this.props.google}
+//       zoom={14}
+//       style={mapStyles}
+//       center={{
+//         lat: 40.73,
+//         lng: -73.93
+//       }}
+//     >
+//      <Marker
+//     title={'The marker`s title will appear as a tooltip.'}
+//     name={'SOMA'}
+//     position={{lat: 40.73, lng: -73.93}} />
+//     </Map>
+//     )
+//   }
+// }
+
+// export default GoogleApiWrapper({
+//   apiKey: "AIzaSyBOgDVbsv4IKyKnKiZnsM0A-6nJSwFa9XY"
+// })(MapContainer)
+
+import React from "react"
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow
+} from "@react-google-maps/api"
+import {formatRelative} from "date-fns"
+
+const libraries = ["places"]
+const mapContainerStyle = {
+  width: "100vw",
+  height: "100vh"
+}
+const center = {
+  lat: 40.73,
+  lng: -73.93
 }
 
-export class MapContainer extends Component {
-  render () {
-    return (
-      <Map 
-      google={this.props.google}
-      zoom={14}
-      style={mapStyles}
-      intialCenter={{
-        lat: 42.966300,
-        lng: -74.582570
-      }}
-    />
-    )
-  }
+export default function App() {
+  const {isLoaded, loadError} = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries
+  })
+  
+  if (loadError) return "Error loading maps"
+  if (!isLoaded) return "Loading maps"
+  return <div>
+    <GoogleMap
+    mapContainerStyle={mapContainerStyle}
+    zoom={8}
+    center={center}
+    ></GoogleMap>
+  </div>
 }
-
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyBOgDVbsv4IKyKnKiZnsM0A-6nJSwFa9XY"
-})(MapContainer)
