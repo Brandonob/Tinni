8eacf4687bd92000eea16c37cf506925313326a7
Jay Fowler <jcafowler@gmail.com>
2020-07-28 21:25:50 -0400

    Add markers via click event

--- frontend/src/features/Map/Map.jsx
+++ frontend/src/features/Map/Map.jsx
@@ -1,38 +1,4 @@
-// import React, {Component} from 'react';
-// import {Map, Marker, GoogleApiWrapper} from "google-maps-react"
-// import './map.css'
-
-// const mapStyles = {
-//   width: "100%",
-//   height: "100%"
-// }
-
-// export class MapContainer extends Component {
-//   render () {
-//     return (
-//       <Map 
-//       google={this.props.google}
-//       zoom={14}
-//       style={mapStyles}
-//       center={{
-//         lat: 40.73,
-//         lng: -73.93
-//       }}
-//     >
-//      <Marker
-//     title={'The marker`s title will appear as a tooltip.'}
-//     name={'SOMA'}
-//     position={{lat: 40.73, lng: -73.93}} />
-//     </Map>
-//     )
-//   }
-// }
-
-// export default GoogleApiWrapper({
-//   apiKey: "AIzaSyBOgDVbsv4IKyKnKiZnsM0A-6nJSwFa9XY"
-// })(MapContainer)
-
-import React from "react"
+import React, {useState} from "react"
 import {
   GoogleMap,
   useLoadScript,
@@ -41,7 +7,12 @@ import {
 } from "@react-google-maps/api"
 import {formatRelative} from "date-fns"
 import mapStyles from "./mapStyles"
-const libraries = ["places"]
+import {receiveSearch} from "../SearchBar/SearchBarSlice"
+import { useDispatch } from "react-redux";
+
+
+
+
 const mapContainerStyle = {
   width: "100vw",
   height: "100vh"
@@ -50,6 +21,7 @@ const center = {
   lat: 40.73,
   lng: -73.93
 }
+
 const options = {
   styles: mapStyles,
   disableDefaultUI: true,
@@ -58,13 +30,9 @@ const options = {
 }
 
 export default function App() {
-  const {isLoaded, loadError} = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries
-  })
-  
-  if (loadError) return "Error loading maps"
-  if (!isLoaded) return "Loading maps"
+
+  const [markers, setMarkers] = useState([])
+  const dispatch = useDispatch()
   return <div>
   <h1>
     Codename Ida {" "} <span role="img" aria-label="world map">🗺</span>
@@ -74,6 +42,15 @@ export default function App() {
     zoom={12}
     center={center}
     options={options}
-    ></GoogleMap>
+    onClick = {(e) => {
+      setMarkers(current => [...current, {
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng(),
+        time: new Date()
+      }])
+    }}
+    >
+      {markers.map(marker => <Marker key={marker.time.toISOString()} position={{lat: marker.lat, lng: marker.lng}}/>)}
+    </GoogleMap>
   </div>
 }
