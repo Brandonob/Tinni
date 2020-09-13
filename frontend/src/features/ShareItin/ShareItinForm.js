6bded0210810a872a44ad41739fa8fdf552b6681
BrandonBrown <brandonbrown@pursuit.org>
2020-09-13 15:00:41 -0400

    clean up code

--- frontend/src/features/ShareItin/ShareItinForm.js
+++ frontend/src/features/ShareItin/ShareItinForm.js
@@ -11,6 +11,7 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
+
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
--- frontend/src/features/UserProfile/ItinCards.js
+++ frontend/src/features/UserProfile/ItinCards.js
@@ -12,6 +12,7 @@ import  itinpic from '../../images/itinpic.png';
 import { useSelector } from "react-redux";
 import { fetchItineraries, selectItins } from '../Itinerary/itinerarySlice'
 import { selectUserID } from '../Users/usersSlice'
+import { ShareItinForm } from '../ShareItin/ShareItinForm'
 
 const useStyles = makeStyles({
     root: {
@@ -27,6 +28,8 @@ export const ItinCards = () => {
 
     const itineraries = useSelector(selectItins);
     const currentUserID = useSelector(selectUserID)
+
+    const [showShareForm, setShowShareForm] = useState(false);
 
     const dispatch = useDispatch();
 
@@ -46,7 +49,7 @@ export const ItinCards = () => {
               <CardMedia
                 className={classes.media}
                 image={itinpic}
-                title="Contemplative Reptile"
+                title="Itinerary Pic"
               />
               <Typography gutterBottom variant="h5" component="h2">
                   {el.title}
@@ -56,7 +59,7 @@ export const ItinCards = () => {
               <Button size="small" color="primary" >
                 Edit
               </Button>
-              <Button size="small" color="primary" >
+              <Button size="small" color="primary" onClick={() => setShowShareForm(true)}>
                 Share
               </Button>
             </CardActions>
@@ -64,6 +67,7 @@ export const ItinCards = () => {
           </Grid>
         )
       })}
+      {showShareForm ? <ShareItinForm/> : null}
     <Grid/>
     </>
   );
