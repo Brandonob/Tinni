e42063b93541171e09a6dd7cc2312b49f03517a3
Dugmar <dugmar.morocho@gmail.com>
2020-09-14 21:26:10 -0400

    update current Itinerary time

--- frontend/src/features/CurrentItinerary/currentItinerarySlice.js
+++ frontend/src/features/CurrentItinerary/currentItinerarySlice.js
@@ -4,29 +4,38 @@ import { createSlice } from "@reduxjs/toolkit";
 // const currentItineraryInfo = useSelector(selectCurrentItinInfo);
 export const currentItinerarySlice = createSlice({
   name: "currentItinerary",
-  initialState: [],
+  initialState: {
+    id: null,
+    Title: "My Itinerary",
+    Date: "",
+    Time: "12:00 ",
+    list: [],
+  },
   reducers: {
     addItemToItin: {
       reducer: (state, action) => {
-        state.push(action.payload);
-        return handleTime(state);
+        state.list.push(action.payload);
+        handleTime(state.list);
+        return state;
       },
       prepare: (body) => {
         return { payload: { body } };
       },
     },
     updateItin: (state, action) => {
-      state.splice(action.payload, 1);
-      return handleTime(state);
+      state.list.splice(action.payload, 1);
+      handleTime(state.list);
+      return state;
     },
     reorder: (state, action) => {
-      const [removed] = state.splice(action.payload.startIndex, 1);
-      state.splice(action.payload.endIndex, 0, removed);
-      return handleTime(state);
+      const [removed] = state.list.splice(action.payload.startIndex, 1);
+      state.list.splice(action.payload.endIndex, 0, removed);
+      handleTime(state.list);
+      return state;
     },
     updateTime: (state, action) => {
-      debugger;
-      state[action.payload.index].body.time.endTime = action.payload.timeEnd;
+      state.list[action.payload.index].body.time.endTime =
+        action.payload.timeEnd;
       return state;
     },
   },
@@ -34,6 +43,7 @@ export const currentItinerarySlice = createSlice({
 
 const handleTime = (state) => {
   let timeintin = "12:00";
+
   state.forEach((el, i) => {
     if (i === 0) {
       let target = new Date("2020-02-20 " + timeintin);
@@ -41,6 +51,7 @@ const handleTime = (state) => {
       target.setMinutes(target.getMinutes() + [el][0].body.time.duration);
       state[i].body.time.endTime =
         target.getHours() + ":" + target.getMinutes();
+      debugger;
     } else {
       let target = new Date("2020-02-20 " + state[i - 1].body.time.endTime);
 
@@ -59,7 +70,7 @@ const handleTime = (state) => {
   return state;
 };
 
-export const selectCurrentItin = (state) => state.currentItinerary;
+export const selectCurrentItin = (state) => state.currentItinerary.list;
 export const {
   addItemToItin,
   updateItin,
